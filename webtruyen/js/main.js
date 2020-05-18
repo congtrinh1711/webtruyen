9934e54887b862ad91f2597046f3826febbb0839
linh <buiquanglinh410@gmail.com>
2020-05-18 21:54:12 +0700

    update

--- webtruyen/js/main.js
+++ webtruyen/js/main.js
@@ -1,13 +1,34 @@
 var menuResponsive = {
    init:function(){
-       this.showMenu()
+       this.showMenu();
+       this.hideOverlay();
+       this.closeMenu()
    },
    showMenu:function(){
        let humburger =  document.querySelector('.header__menu .humburger');
        let nav = document.querySelector('.nav-mobile');
+       let overlay = document.querySelector('.overlay');
        humburger.addEventListener('click',() => {
            nav.classList.toggle('show');
+           overlay.classList.add('show');
        })
+   },
+   hideOverlay:function(){
+      let nav = document.querySelector('.nav-mobile');
+      let overlay = document.querySelector('.overlay');
+      overlay.addEventListener('click',() => {
+        nav.classList.remove('show');
+        overlay.classList.remove('show');
+    })
+   },
+   closeMenu:function(){
+    let nav = document.querySelector('.nav-mobile');
+    let overlay = document.querySelector('.overlay');
+    let button = document.querySelector('.button-close');
+    button.addEventListener('click',() => {
+        nav.classList.remove('show');
+        overlay.classList.remove('show');
+    })
    }
 }
 menuResponsive.init()
